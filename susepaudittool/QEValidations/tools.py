af9035fb8c3624b068b95b361a5706184080213a
Marcelo Majer Franceschini <marcelo.franceschini@br.gt.lan>
2019-09-24 18:50:01 -0300

    Fixing small stuff

--- susepaudittool/QEValidations/tools.py
+++ susepaudittool/QEValidations/tools.py
@@ -1,27 +1,31 @@
 import os
 
 # TOOLS
+
+
 def make_command(column, nome_arquivo, n, QE):
     command = 'INSERT INTO "QE" ("File Name", "Line", TX) \
         VALUES ("filex", linex, 1);'
 
-    return command.replace("TX", column).replace(
-        "filex", nome_arquivo).replace("linex", str(n)).replace("QE", QE)
-    
+    return (
+        command.replace("TX", column)
+        .replace("filex", nome_arquivo)
+        .replace("linex", str(n))
+        .replace("QE", QE)
+    )
+
+
 def get_moedas():
     moedas = []
-    with open(os.path.abspath(os.path.join("susepaudittool","QEValidations", "moedas.txt"))) as txt:
+    with open(
+        os.path.abspath(
+            os.path.join(os.path.abspath(os.path.dirname(__file__)), "moedas.txt")
+        )
+    ) as txt:
         for linha in txt.readlines():
             moedas.append(linha.strip())
     return moedas
 
-
-def get_ramos():
-    ramos = []
-    with open(os.path.abspath(os.path.join("TXTs", "ramos.txt"))) as txt:
-        for linha in txt.readlines():
-            ramos.append(linha.strip())
-    return ramos
 
 def resolve42x(line):
     if len(line) == 70:
