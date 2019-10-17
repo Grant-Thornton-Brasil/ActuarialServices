e330a4acf86782a7825b229c295ee0b3c3c67c56
Marcelo Majer Franceschini <marcelo.franceschini@br.gt.lan>
2019-10-17 14:57:57 -0300

    Vars 376; 377 updated
    Maybe it's taking longer but it's more readable and easier to fix

--- susepaudittool/QEMaths/Insurance/maths376.py
+++ susepaudittool/QEMaths/Insurance/maths376.py
@@ -12,74 +12,80 @@ class maths_376():
         self.df.fillna(0.0, inplace=True)
 
     def run(self, linha):
+        # VARS
+        MRFMESANO = linha[12:20]
+        TPMOID = linha[23:27]
+        CMPID = linha[27:31]
+        ESRVALORMOV = float(linha[67:80])
+
         # Cruzamento 1
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1001":
-            self.df["Cruzamento 1 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1001":
-            self.df["Cruzamento 1 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1001":
+            self.df["Cruzamento 1 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1001":
+            self.df["Cruzamento 1 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 2
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1002":
-            self.df["Cruzamento 2 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1002":
-            self.df["Cruzamento 2 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1002":
+            self.df["Cruzamento 2 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1002":
+            self.df["Cruzamento 2 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 3
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1003":
-            self.df["Cruzamento 3 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1003":
-            self.df["Cruzamento 3 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1003":
+            self.df["Cruzamento 3 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1003":
+            self.df["Cruzamento 3 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 4
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1006":
-            self.df["Cruzamento 4 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1006":
-            self.df["Cruzamento 4 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1006":
+            self.df["Cruzamento 4 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1006":
+            self.df["Cruzamento 4 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 5
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1007":
-            self.df["Cruzamento 5 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1007":
-            self.df["Cruzamento 5 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1007":
+            self.df["Cruzamento 5 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1007":
+            self.df["Cruzamento 5 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 6
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1008":
-            self.df["Cruzamento 6 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1008":
-            self.df["Cruzamento 6 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1008":
+            self.df["Cruzamento 6 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1008":
+            self.df["Cruzamento 6 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 7
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1012":
-            self.df["Cruzamento 7 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1012":
-            self.df["Cruzamento 7 - 376"][linha[12:20]] -= float(linha[67:80])
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1011":
-            self.df["Cruzamento 7 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1011":
-            self.df["Cruzamento 7 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1012":
+            self.df["Cruzamento 7 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1012":
+            self.df["Cruzamento 7 - 376"][MRFMESANO] -= ESRVALORMOV
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1011":
+            self.df["Cruzamento 7 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1011":
+            self.df["Cruzamento 7 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 8
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1013":
-            self.df["Cruzamento 8 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1013":
-            self.df["Cruzamento 8 - 376"][linha[12:20]] -= float(linha[67:80])
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1014":
-            self.df["Cruzamento 8 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1014":
-            self.df["Cruzamento 8 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1013":
+            self.df["Cruzamento 8 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1013":
+            self.df["Cruzamento 8 - 376"][MRFMESANO] -= ESRVALORMOV
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1014":
+            self.df["Cruzamento 8 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1014":
+            self.df["Cruzamento 8 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 9
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1004":
-            self.df["Cruzamento 9 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1004":
-            self.df["Cruzamento 9 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1004":
+            self.df["Cruzamento 9 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1004":
+            self.df["Cruzamento 9 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 10
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1009":
-            self.df["Cruzamento 10 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1009":
-            self.df["Cruzamento 10 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1009":
+            self.df["Cruzamento 10 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1009":
+            self.df["Cruzamento 10 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 11
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1005":
-            self.df["Cruzamento 11 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1005":
-            self.df["Cruzamento 11 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1005":
+            self.df["Cruzamento 11 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1005":
+            self.df["Cruzamento 11 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 12
-        if linha[23:27] in ["0001", "0002", "0006"] and linha[27:31] == "1010":
-            self.df["Cruzamento 12 - 376"][linha[12:20]] += float(linha[67:80])
-        if linha[23:27] == "0005" and linha[27:31] == "1010":
-            self.df["Cruzamento 12 - 376"][linha[12:20]] -= float(linha[67:80])
+        if TPMOID in ["0001", "0002", "0006"] and CMPID == "1010":
+            self.df["Cruzamento 12 - 376"][MRFMESANO] += ESRVALORMOV
+        if TPMOID == "0005" and CMPID == "1010":
+            self.df["Cruzamento 12 - 376"][MRFMESANO] -= ESRVALORMOV
         # Cruzamento 13
             self.df["Cruzamento 13 - 376"] = self.df["Cruzamento 1 - 376"] + \
                 self.df["Cruzamento 2 - 376"] - self.df["Cruzamento 3 - 376"] \
--- susepaudittool/QEMaths/Insurance/maths377.py
+++ susepaudittool/QEMaths/Insurance/maths377.py
@@ -12,35 +12,40 @@ class maths_377():
         self.df.fillna(0.0, inplace=True)
 
     def run(self, linha):
+        # VARS
+        MRFMESANO = linha[12:20]
+        CMPID = linha[23:27]
+        ESLVALORMOV = float(linha[63:76])
+
         # Cruzamento 1
-        if int(linha[23:27]) == 1015:
-            self.df["Cruzamento 1 - 377"][linha[12:20]] += float(linha[63:76])
+        if CMPID == "1015":
+            self.df["Cruzamento 1 - 377"][MRFMESANO] += ESLVALORMOV
         # Cruzamento 2
-        if int(linha[23:27]) == 1016:
-            self.df["Cruzamento 2 - 377"][linha[12:20]] += float(linha[63:76])
+        if CMPID == "1016":
+            self.df["Cruzamento 2 - 377"][MRFMESANO] += ESLVALORMOV
         # Cruzamento 3
-        if int(linha[23:27]) == 1017:
-            self.df["Cruzamento 3 - 377"][linha[12:20]] += float(linha[63:76])
+        if CMPID == "1017":
+            self.df["Cruzamento 3 - 377"][MRFMESANO] += ESLVALORMOV
         # Cruzamento 4
-        if int(linha[23:27]) == 1018:
-            self.df["Cruzamento 4 - 377"][linha[12:20]] += float(linha[63:76])
+        if CMPID == "1018":
+            self.df["Cruzamento 4 - 377"][MRFMESANO] += ESLVALORMOV
         # Cruzamento 5
-        if int(linha[23:27]) == 1020:
-            self.df["Cruzamento 5 - 377"][linha[12:20]] += float(linha[63:76])
+        if CMPID == "1020":
+            self.df["Cruzamento 5 - 377"][MRFMESANO] += ESLVALORMOV
         # Cruzamento 6
-        if int(linha[23:27]) == 1021:
-            self.df["Cruzamento 6 - 377"][linha[12:20]] += float(linha[63:76])
+        if CMPID == "1021":
+            self.df["Cruzamento 6 - 377"][MRFMESANO] += ESLVALORMOV
         # Cruzamento 7
-        if int(linha[23:27]) == 1022:
-            self.df["Cruzamento 7 - 377"][linha[12:20]] += float(linha[63:76])
+        if CMPID == "1022":
+            self.df["Cruzamento 7 - 377"][MRFMESANO] += ESLVALORMOV
         # Cruzamento 8
-        if int(linha[23:27]) == 1023:
-            self.df["Cruzamento 8 - 377"][linha[12:20]] += float(linha[63:76])
+        if CMPID == "1023":
+            self.df["Cruzamento 8 - 377"][MRFMESANO] += ESLVALORMOV
         # Cruzamento 9
-        if int(linha[23:27]) == 1019:
-            self.df["Cruzamento 9 - 377"][linha[12:20]] += float(linha[63:76])
-        if int(linha[23:27]) == 1024:
-            self.df["Cruzamento 9 - 377"][linha[12:20]] += float(linha[63:76])
+        if CMPID == "1019":
+            self.df["Cruzamento 9 - 377"][MRFMESANO] += ESLVALORMOV
+        if CMPID == "1024":
+            self.df["Cruzamento 9 - 377"][MRFMESANO] += ESLVALORMOV
         # Cruzamento 10
-        if int(linha[23:27]) == 1025:
-            self.df["Cruzamento 10 - 377"][linha[12:20]] += float(linha[63:76])
+        if CMPID == "1025":
+            self.df["Cruzamento 10 - 377"][MRFMESANO] += ESLVALORMOV
